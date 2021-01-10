1fec521f5c643991c182e3005aefeee865ed46c3
Sameer Kulkarni <sameermk97@gmail.com>
2021-01-10 15:01:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+
+//DB config
+const db = require('./config/keys').MongoURI;
+
+//connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Bodyparser
+app.use(express.urlencoded({extended: false}));
+
+//Express session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  }));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+  //connect flash
+  app.use(flash());
+
+
+  //global vars
+  app.use((req, res, next ) =>{
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  })
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server sarted on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI: 'mongodb://Mongo:sdmcet%40123@logindb-shard-00-00.pkmxi.mongodb.net:27017,logindb-shard-00-01.pkmxi.mongodb.net:27017,logindb-shard-00-02.pkmxi.mongodb.net:27017/RegisteredUsers?ssl=true&replicaSet=atlas-w5qt0j-shard-0&authSource=admin&retryWrites=true&w=majority'
+    
+}
